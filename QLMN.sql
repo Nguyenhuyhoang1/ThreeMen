06b31bbe2e2c224a5a116db2901b509dee179b4f
Huy Hoang <91108106+Nguyenhuyhoang1@users.noreply.github.com>
2021-11-11 07:54:22 +0700

    Add files via upload

--- QLMN.sql
+++ QLMN.sql
@@ -1,147 +1,124 @@
-/*
-Created		7/10/2021
-Modified		10/5/2021
-Project		
-Model			
-Company		
-Author		
-Version		
-Database		MS SQL 2005 
-*/
 Create database ThongTinMN
-go 
+go
 use ThongTinMN
 go
-
-
 Create table [Lop]
 (
-	[MaLop] Nvarchar(10) NOT NULL,
-	[TenLop] Nvarchar(30) NULL,
-	[Siso] Integer NULL,
+	[MaLop] Char(5) NOT NULL,
+	[TenLop] Nvarchar(10) NULL,
+	[soluong] Integer NULL,
+	[Namhoc] Char(9) NULL,
 Primary Key ([MaLop])
 ) 
 go
 
 Create table [PH]
 (
-	[MaPH] Nvarchar(10) NOT NULL,
-	[TenPH] Nvarchar(40) NULL,
+	[MaPH] Char(5) NOT NULL,
+	[TenPH] Nvarchar(30) NULL,
 	[NgaySinh] Datetime NULL,
-	[tuoi] Integer NULL,
 	[gioitinh] Nvarchar(5) NULL,
 	[dantoc] Nvarchar(15) NULL,
-	[tongiao] Nvarchar(15) NULL,
-	[SoDTPH] Nvarchar(15) NULL,
-	[diachi] Nvarchar(50) NULL,
+	[SoDTPH1] Char(10) NULL,
+	[SoDTPH2] Char(10) NULL,
+	[diachi] Nvarchar(70) NULL,
 	[noicongtac] Nvarchar(30) NULL,
 	[nghenghiep] Nvarchar(30) NULL,
-	[email] Nvarchar(30) NULL,
+	[email] Char(40) NULL,
 Primary Key ([MaPH])
 ) 
 go
 
 Create table [GiaoVien]
 (
-	[MaGV] Nvarchar(10) NOT NULL,
-	[TenGV] Nvarchar(40) NULL,
+	[MaGV] Char(5) NOT NULL,
+	[TenGV] Nvarchar(30) NULL,
 	[gioitinh] Nvarchar(5) NULL,
 	[ngaysinh] Datetime NULL,
-	[soDTGV] Nvarchar(15) NULL,
+	[soDTGV] Char(10) NULL,
 	[TrinhDo] Nvarchar(20) NULL,
 	[chuyenmon] Nvarchar(30) NULL,
-	[noidaotao] Nvarchar(50) NULL,
-	[diachi] Nvarchar(50) NULL,
-	[email] Nvarchar(30) NULL,
+	[diachi] Nvarchar(70) NULL,
+	[email] Nvarchar(40) NULL,
 Primary Key ([MaGV])
 ) 
 go
 
 Create table [CBYT]
 (
-	[MaCBYT] Nvarchar(10) NOT NULL,
-	[tenCBYT] Nvarchar(40) NULL,
+	[MaCBYT] Char(5) NOT NULL,
+	[tenCBYT] Nvarchar(30) NULL,
+	[ngaysinh] Datetime NULL,
+	[gioitinh] Nvarchar(5) NULL,
+	[soDTCB] Char(10) NULL,
 	[trinhdo] Nvarchar(20) NULL,
 	[chuyenmon] Nvarchar(20) NULL,
-	[diachi] Nvarchar(50) NULL,
-	[soDTCB] Nvarchar(15) NULL,
-	[email] Nvarchar(30) NULL,
+	[diachi] Nvarchar(70) NULL,	
+	[email] Char(40) NULL
+		
 Primary Key ([MaCBYT])
 ) 
 go
 
 Create table [HS]
 (
-	[MaHS] Nvarchar(10) NOT NULL,
-	[TenHS] Nvarchar(40) NULL,
-	[Tuoi] Integer NULL,
-	[ngaysinh] Datetime NULL,
+	[MaHS] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
+	[TenHS] Nvarchar(30) NULL,
+	[MaLop] Char(5) NOT NULL,	
+	[ngaysinh] Date NULL,
 	[gioitinh] Nvarchar(5) NULL,
-	[dantoc] Nvarchar(15) NULL,
-	[diachi] Nvarchar(30) NULL,
-	[ghichusuckhoe] Nvarchar(50) NULL,
+	[ghichu] Nvarchar(50) NULL,
 Primary Key ([MaHS])
 ) 
 go
 
 Create table [DonNhapHoc]
 (
-	[MaDon] Nvarchar(10) NOT NULL,
-	[MaCBVP] Nvarchar(10) NOT NULL,
-	[MaPH] Nvarchar(10) NOT NULL,
+	[MaDon] Char(5) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
 	[NgayDangKy] Datetime NULL,
 	[Loptuoi] Nvarchar(10) NULL,
 Primary Key ([MaDon])
 ) 
 go
 
-Create table [PhieuXeplop]
-(
-	[MaPXL] Nvarchar(10) NOT NULL,
-	[MaLop] Nvarchar(10) NOT NULL,
-	[MaHS] Nvarchar(10) NOT NULL,
-	[MaCBVP] Nvarchar(10) NOT NULL,
-	[Namhoc] Datetime NULL,
-Primary Key ([MaPXL])
-) 
-go
-
 Create table [Phieudiemdanh]
 (
-	[MaPDD] Nvarchar(10) NOT NULL,
-	[MaLop] Nvarchar(10) NOT NULL,
-	[MaHS] Nvarchar(10) NOT NULL,
-	[MaGV] Nvarchar(10) NOT NULL,
+	[MaPDD] Char(5) NOT NULL,
+	[MaGV] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
 	[Ngaydiemdanh] Datetime NULL,
 	[Tinhtrangvang] Nvarchar(10) NULL,
-	[Lydovang] Nvarchar(10) NULL,
+	[Lydo] Nvarchar(20) NULL,
 Primary Key ([MaPDD])
 ) 
 go
 
 Create table [BienLai]
 (
-	[MaBL] Nvarchar(10) NOT NULL,
-	[MaCBVP] Nvarchar(10) NOT NULL,
-	[MaPH] Nvarchar(10) NOT NULL,
+	[MaBL] Char(10) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
 	[ngaythu] Datetime NULL,
-	[Tienhocphi] Money NULL,
-	[Tienan] Money NULL,
-	[Tienphuthu] Money NULL,
-	[tongtienhocphi] Money NULL,
+	[Tienhocphi] int NULL,
+	[Tienan] int NULL,
+	[Tienphuthu] int NULL,
+	[tongtienhocphi] int NULL,
 Primary Key ([MaBL])
 ) 
 go
 
 Create table [PhieuKhamSK]
 (
-	[MaPK] Nvarchar(10) NOT NULL,
-	[MaLop] Nvarchar(10) NOT NULL,
-	[MaHS] Nvarchar(10) NOT NULL,
+	[MaPK] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
 	[ngaykham] Datetime NULL,
-	[namhoc] Nvarchar(10) NULL,
-	[Chieucao] Nvarchar(10) NULL,
-	[Cannang] Nvarchar(10) NULL,
+	[namhoc] Integer NULL,
+	[Chieucao] Char(10) NULL,
+	[Cannang] Char(10) NULL,
 	[Taimuihong] Nvarchar(100) NULL,
 	[Mat] Nvarchar(100) NULL,
 	[Xuong] Nvarchar(100) NULL,
@@ -150,69 +127,178 @@ Primary Key ([MaPK])
 ) 
 go
 
-Create table [CBVanPhong]
+Create table [NV]
 (
-	[MaCBVP] Nvarchar(10) NOT NULL,
-	[TenCBVP] Nvarchar(40) NULL,
+	[MaNV] Char(5) NOT NULL,
+	[TenNV] Nvarchar(30) NULL,
+	[Gioitinh] Nvarchar(5) NULL,
+	[ngaysinh] Date NULL,
 	[chucvu] Nvarchar(20) NULL,
 	[trinhdo] Nvarchar(20) NULL,
-	[chuyenmon] Nvarchar(20) NULL,
-	[diachi] Nvarchar(50) NULL,
-	[email] Nvarchar(30) NULL,
-	[sodt] Nvarchar(15) NULL,
-Primary Key ([MaCBVP])
-) 
-go
-
-Create table [CT_Lop]
-(
-	[MaLop] Nvarchar(10) NOT NULL,
-	[MaHS] Nvarchar(10) NOT NULL,
-Primary Key ([MaLop],[MaHS])
+	[diachi] Nvarchar(70) NULL,
+	[email] Nchar(40) NULL,
+	[sodt] Char(10) NULL,
+Primary Key ([MaNV])
 ) 
 go
 
 Create table [CT_Kham]
 (
-	[MaCBYT] Nvarchar(10) NOT NULL,
-	[MaPK] Nvarchar(10) NOT NULL,
+	[MaCBYT] Char(5) NOT NULL,
+	[MaPK] Char(5) NOT NULL,
+	[SoLuongKham] Integer NULL,
 Primary Key ([MaCBYT],[MaPK])
 ) 
 go
 
 Create table [PhieuBaoAn]
 (
-	[MaPhieu] Nvarchar(10) NOT NULL,
-	[MaGV] Nvarchar(10) NOT NULL,
-	[MaCBVP] Nvarchar(10) NOT NULL,
-	[MaCBCD] Nvarchar(10) NOT NULL,
+	[MaPhieu] Char(5) NOT NULL,
+	[MaGV] Char(5) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[NgayBaoAn] datetime NULL,
 	[SoXuatAn] Integer NULL,
-	[NgayBaoAn] Datetime NULL,
+	
 Primary Key ([MaPhieu])
 ) 
 go
 
-Create table [CBCD]
+Create table [tbUser]
 (
-	[MaCBCD] Nvarchar(10) NOT NULL,
-	[TenCBCD] Nvarchar(40) NULL,
-	[ChucVu] Nvarchar(20) NULL,
-	[TrinhDo] Nvarchar(20) NULL,
-	[ChuyenMon] Nvarchar(20) NULL,
-	[DiaChi] Nvarchar(50) NULL,
-	[Email] Nvarchar(3
-	0) NULL,
-	[SoDT] Nvarchar(15) NULL,
-Primary Key ([MaCBCD])
+	[UserName] Varchar(50) NOT NULL,
+	[Pass] Nvarchar(20) NOT NULL,
+	[HoTen] Nvarchar(30) NULL,
+	[Quyen] Nvarchar(10) NULL,
+Primary Key ([UserName])
 ) 
 go
-
-
-Alter table [CT_Lop] add  foreign key([MaLop]) references [Lop] ([MaLop])  on update no action on delete no action 
+Create table [NVCU]
+(
+	[MaNV] Char(5) NOT NULL,
+	[TenNV] Nvarchar(30) NULL,
+	[Gioitinh] Nvarchar(5) NULL,
+	[ngaysinh] Date NULL,
+	[chucvu] Nvarchar(20) NULL,
+	[trinhdo] Nvarchar(20) NULL,
+	[diachi] Nvarchar(70) NULL,
+	[email] Nchar(40) NULL,
+	[sodt] Char(10) NULL
+) 
+go
+Create table [GiaoVienCU]
+(
+	[MaGV] Char(5) NOT NULL,
+	[TenGV] Nvarchar(30) NULL,
+	[gioitinh] Nvarchar(5) NULL,
+	[ngaysinh] Datetime NULL,
+	[soDTGV] Char(10) NULL,
+	[TrinhDo] Nvarchar(20) NULL,
+	[chuyenmon] Nvarchar(30) NULL,
+	[diachi] Nvarchar(70) NULL,
+	[email] Nvarchar(40) NULL
+) 
+go
+Create table [CBYTcu]
+(
+	[MaCBYT] Char(5) NOT NULL,
+	[tenCBYT] Nvarchar(30) NULL,
+	[trinhdo] Nvarchar(20) NULL,
+	[chuyenmon] Nvarchar(20) NULL,
+	[diachi] Nvarchar(70) NULL,
+	[soDTCB] Char(10) NULL,
+	[email] Char(40) NULL,
+	[gioitinh] Nvarchar(5) NULL,
+	[ngaysinh] Datetime NULL
+) 
+go
+Create table [PhieuBaoAnCu]
+(
+	[MaPhieu] Char(5) NOT NULL,
+	[MaGV] Char(5) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[NgayBaoAn] char NULL
+	[SoXuatAn] Integer NULL,
+) 
+go
+Create table [BienLaiCu]
+(
+	[MaBL] Char(10) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
+	[ngaythu] Datetime NULL,
+	[Tienhocphi] int NULL,
+	[Tienan] int NULL,
+	[Tienphuthu] int NULL,
+	[tongtienhocphi] int NULL
+) 
+go
+Create table [DonNhapHocCu]
+(
+	[MaDon] Char(5) NOT NULL,
+	[MaNV] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
+	[NgayDangKy] Datetime NULL,
+	[Loptuoi] Nvarchar(10) NULL
+) 
+go
+Create table [PhieuKhamSKCu]
+(
+	[MaPK] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
+	[ngaykham] Datetime NULL,
+	[namhoc] Integer NULL,
+	[Chieucao] Char(10) NULL,
+	[Cannang] Char(10) NULL,
+	[Taimuihong] Nvarchar(100) NULL,
+	[Mat] Nvarchar(100) NULL,
+	[Xuong] Nvarchar(100) NULL,
+	[Rang] Nvarchar(100) NULL
+)
+go
+Create table [PhieudiemdanhCu]
+(
+	[MaPDD] Char(5) NOT NULL,
+	[MaGV] Char(5) NOT NULL,
+	[MaHS] Char(5) NOT NULL,
+	[Ngaydiemdanh] Datetime NULL,
+	[Tinhtrangvang] Nvarchar(10) NULL,
+	[Lydo] Nvarchar(20) NULL
+) 
+go
+Create table [HSCu]
+(
+	[MaHS] Char(5) NOT NULL,
+	[MaPH] Char(5) NOT NULL,
+	[MaLop] Char(5) NOT NULL,
+	[TenHS] Nvarchar(30) NULL,
+	[ngaysinh] Date NULL,
+	[gioitinh] Nvarchar(5) NULL,
+	[ghichu] Nvarchar(50) NULL
+) 
+go
+Create table [PHCu]
+(
+	[MaPH] Char(5) NOT NULL,
+	[TenPH] Nvarchar(30) NULL,
+	[NgaySinh] Datetime NULL,
+	[gioitinh] Nvarchar(5) NULL,
+	[dantoc] Nvarchar(15) NULL,
+	[SoDTPH1] Char(10) NULL,
+	[SoDTPH2] Char(10) NULL,
+	[diachi] Nvarchar(70) NULL,
+	[noicongtac] Nvarchar(30) NULL,
+	[nghenghiep] Nvarchar(30) NULL,
+	[email] Char(40) NULL
+) 
+go
+Alter table [HS] add  foreign key([MaLop]) references [Lop] ([MaLop])  on update no action on delete no action 
 go
 Alter table [DonNhapHoc] add  foreign key([MaPH]) references [PH] ([MaPH])  on update no action on delete no action 
 go
 Alter table [BienLai] add  foreign key([MaPH]) references [PH] ([MaPH])  on update no action on delete no action 
+go
+Alter table [HS] add  foreign key([MaPH]) references [PH] ([MaPH])  on update no action on delete no action 
 go
 Alter table [Phieudiemdanh] add  foreign key([MaGV]) references [GiaoVien] ([MaGV])  on update no action on delete no action 
 go
@@ -220,33 +306,30 @@ Alter table [PhieuBaoAn] add  foreign key([MaGV]) references [GiaoVien] ([MaGV])
 go
 Alter table [CT_Kham] add  foreign key([MaCBYT]) references [CBYT] ([MaCBYT])  on update no action on delete no action 
 go
-Alter table [CT_Lop] add  foreign key([MaHS]) references [HS] ([MaHS])  on update no action on delete no action 
+Alter table [PhieuKhamSK] add  foreign key([MaHS]) references [HS] ([MaHS])  on update no action on delete no action 
+go
+Alter table [DonNhapHoc] add  foreign key([MaHS]) references [HS] ([MaHS])  on update no action on delete no action 
+go
+Alter table [Phieudiemdanh] add  foreign key([MaHS]) references [HS] ([MaHS])  on update no action on delete no action 
 go
 Alter table [CT_Kham] add  foreign key([MaPK]) references [PhieuKhamSK] ([MaPK])  on update no action on delete no action 
 go
-Alter table [PhieuXeplop] add  foreign key([MaCBVP]) references [CBVanPhong] ([MaCBVP])  on update no action on delete no action 
+Alter table [BienLai] add  foreign key([MaNV]) references [NV] ([MaNV])  on update no action on delete no action 
 go
-Alter table [BienLai] add  foreign key([MaCBVP]) references [CBVanPhong] ([MaCBVP])  on update no action on delete no action 
+Alter table [DonNhapHoc] add  foreign key([MaNV]) references [NV] ([MaNV])  on update no action on delete no action 
 go
-Alter table [DonNhapHoc] add  foreign key([MaCBVP]) references [CBVanPhong] ([MaCBVP])  on update no action on delete no action 
-go
-Alter table [PhieuBaoAn] add  foreign key([MaCBVP]) references [CBVanPhong] ([MaCBVP])  on update no action on delete no action 
-go
-Alter table [Phieudiemdanh] add  foreign key([MaLop],[MaHS]) references [CT_Lop] ([MaLop],[MaHS])  on update no action on delete no action 
-go
-Alter table [PhieuKhamSK] add  foreign key([MaLop],[MaHS]) references [CT_Lop] ([MaLop],[MaHS])  on update no action on delete no action 
-go
-Alter table [PhieuXeplop] add  foreign key([MaLop],[MaHS]) references [CT_Lop] ([MaLop],[MaHS])  on update no action on delete no action 
-go
-Alter table [PhieuBaoAn] add  foreign key([MaCBCD]) references [CBCD] ([MaCBCD])  on update no action on delete no action 
+Alter table [PhieuBaoAn] add  foreign key([MaNV]) references [NV] ([MaNV])  on update no action on delete no action 
 go
 
 
 Set quoted_identifier on
 go
-
-
 Set quoted_identifier off
 go
-
+select * from BienLaiCu
+Insert Into BienLaiCu select * from BienLai where MaNV = 'NV008'
+Insert into PhieuBaoAnCu(MaPhieu,MaGV,MaNV,NgayBaoAn ,SoXuatAn ) select *  from  PhieuBaoAn where MaNV = 'NV008'
+Delete from PhieuBaoAn where MaNV = 'NV008'
+go
+select * from BienLai where MONTH (ngaythu) = '11'
 
